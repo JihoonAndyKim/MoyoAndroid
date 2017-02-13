88f4a71308cd3bf9905584485aeb346a56cbc6e9
Andy Kim <andy.kim@workday.com>
2017-02-13 10:15:28 -0700

    first commit

---
+++ java/no/nordicsemi/android/nrftoolbox/pro/ProfileActivity.java
@@ -0,0 +1,33 @@
+package no.nordicsemi.android.nrftoolbox.pro;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import no.nordicsemi.android.nrftoolbox.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
