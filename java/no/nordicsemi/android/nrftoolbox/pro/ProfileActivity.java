e8276238eca71d9c3f7b4f8d42be08d3b7f35f3a
Andy Kim <andy.kim@workday.com>
2017-02-14 09:26:37 -0700

    Changing Profile UI

--- java/no/nordicsemi/android/nrftoolbox/pro/ProfileActivity.java
+++ java/no/nordicsemi/android/nrftoolbox/pro/ProfileActivity.java
@@ -1,5 +1,6 @@
 package no.nordicsemi.android.nrftoolbox.pro;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import no.nordicsemi.android.nrftoolbox.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    public static final String PREFS_NAME = "MyPrefsFile";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,11 @@ public class ProfileActivity extends AppCompatActivity {
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean silent = settings.getBoolean("silentMode", false);
+
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -29,5 +37,19 @@ public class ProfileActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+
+        // Commit the edits!
+        editor.commit();
+    }
+
 
 }
